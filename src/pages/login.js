57607e0ae971f2121a90b4848c4225e53b369efb
Alvin widinugroho <alvinwidinugroho@gmail.com>
2024-06-15 20:59:17 +0700

    ini dulu

---
+++ src/pages/login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Login() {
+    return (
+        <div style={{ marginLeft: "22%", position: "absolute"}} className="w-9/12">
+
+      </div>
+    );
+}
+
+export default Login;
